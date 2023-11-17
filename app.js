8917aff8b7412fef0cc5917f2226dcb53e4380ff
Azamat Gaparov <agaparovkg@gmail.com>
2023-11-16 17:55:41 -0800

    small change

--- app.js
+++ app.js
@@ -5,3 +5,32 @@ const showExerciseBtn = document.querySelector("#exercise");
 const main = document.querySelector("main");
 const exerciseSection = document.querySelector(".exercise-section");
 let infoEl = document.querySelector("#info");
+
+let countries = [];
+
+async function fetchData1() {
+  const url = "https://restcountries.com/v3.1/all";
+
+  // async/await
+
+  //   const resp = await fetch(url); // methods: GET, DELETE, PATCH, POST,...
+  const resp = await axios.get(url); // methods: GET, DELETE, PATCH, POST,...
+  const data = await resp.json();
+
+  console.log(data);
+}
+
+// CRUD: CREATE, READ, UPDATE, DELETE
+
+const fetchData2 = async () => {
+  const url = "https://restcountries.com/v3.1/all";
+
+  // async/await
+  const resp = await fetch(url); // methods: GET, DELETE, PATCH, POST,...
+  const data = await resp.json();
+
+  console.log(data);
+};
+
+fetchData1();
+fetchData2();
