5c336ad1c7823aed1b8ee81134bf188e55bd20e4
Azamat Gaparov <agaparovkg@gmail.com>
2023-11-16 19:06:10 -0800

    small change

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 import { getCountries } from "./getCountries.js";
 
+const spinner = document.querySelector("#spinner");
 const prevBtn = document.querySelector("#previous");
 const nextBtn = document.querySelector("#next");
-const spinner = document.querySelector("#spinner");
 const showExerciseBtn = document.querySelector("#exercise");
 const main = document.querySelector("main");
 const exerciseSection = document.querySelector(".exercise-section");
 let infoEl = document.querySelector("#info");
 
 let countries = [];
+let currIndex = 0;
 
 // 2 options to fetch data: fetch, axios
 
@@ -20,8 +21,32 @@ getCountries().then((data) => {
 
   setTimeout(() => {
     spinner.style.display = "none";
+    infoEl.style.display = "flex";
+    displayCountryInfo(countries[currIndex]);
   }, 1000);
 });
+
+function displayCountryInfo(country) {
+  const {
+    capital,
+    region,
+    currencies,
+    population,
+    name: { common },
+    flags: { png },
+  } = country;
+
+  const firstCurr = Object.keys(currencies)[0];
+
+  infoEl.innerHTML = `
+          <img src="${png}" alt="" />
+          <p>Country: <strong>${common}</strong></p>
+          <p>Population: <strong>${population.toLocaleString()}</strong></p>
+          <p>Capital city: <strong>${capital[0]}</strong></p>
+          <p>Region: <strong>${region}</strong></p>
+          <p>Currency: <strong>1 USD = ? ${firstCurr}</strong></p>
+        `;
+}
 
 // async/await
 // async function fetchData() {
