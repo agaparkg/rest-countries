41cbfd645c9ff2296ad05dd4cdcba68b9b6dc3c6
Azamat Gaparov <agaparovkg@gmail.com>
2023-11-16 19:38:11 -0800

    small change

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import { getCountries } from "./getCountries.js";
 
 const spinner = document.querySelector("#spinner");
+const main = document.querySelector("main");
+let infoEl = document.querySelector("#info");
 const prevBtn = document.querySelector("#previous");
 const nextBtn = document.querySelector("#next");
-const showExerciseBtn = document.querySelector("#exercise");
-const main = document.querySelector("main");
-const exerciseSection = document.querySelector(".exercise-section");
-let infoEl = document.querySelector("#info");
+// const showExerciseBtn = document.querySelector("#exercise");
+// const exerciseSection = document.querySelector(".exercise-section");
 
 let countries = [];
 let currIndex = 0;
@@ -23,6 +23,7 @@ getCountries().then((data) => {
     spinner.style.display = "none";
     infoEl.style.display = "flex";
     displayCountryInfo(countries[currIndex]);
+    displayAllCountries(countries);
   }, 1000);
 });
 
@@ -47,6 +48,52 @@ function displayCountryInfo(country) {
           <p>Currency: <strong>1 USD = ? ${firstCurr}</strong></p>
         `;
 }
+
+function displayAllCountries(countries) {
+  main.innerHTML = "";
+
+  for (let i = 0; i < countries.length; i++) {
+    const {
+      flags: { png: countryFlag },
+      name: { common: countryName },
+      ccn3: id,
+    } = countries[i];
+
+    let selectedClass = currIndex === i ? "selected" : "";
+
+    const single = `<div class="${selectedClass}">
+                        <p><strong>${countryName}</strong></p>
+                        <img id="${id}" src="${countryFlag}" alt="country-${id}" />
+                    </div>`;
+
+    main.innerHTML += single;
+  }
+
+  for (let div of main.children) {
+    div.children[1].addEventListener("click", singleCountryClick); // [p, img]
+  }
+}
+
+function singleCountryClick(e) {
+  const id = e.target.id;
+  const index = countries.findIndex((country) => country.ccn3 === id);
+  const parentDiv = e.target.parentElement;
+  currIndex = index;
+
+  const oldSelectedClassEl = document.querySelector(".selected");
+  oldSelectedClassEl.classList.remove("selected");
+  parentDiv.classList.add("selected");
+
+  displayCountryInfo(countries[currIndex]);
+}
+
+prevBtn.addEventListener("click", (e) => {
+  console.log(e.target.id);
+});
+
+nextBtn.addEventListener("click", (e) => {
+  console.log(e.target.id);
+});
 
 // async/await
 // async function fetchData() {
