60676321226f69e98418d047158dafe7a8014d7e
Azamat Gaparov <agaparovkg@gmail.com>
2023-11-16 17:37:43 -0800

    small change

---
+++ app.js
@@ -0,0 +1,7 @@
+const prevBtn = document.querySelector("#previous");
+const nextBtn = document.querySelector("#next");
+const spinner = document.querySelector("#spinner");
+const showExerciseBtn = document.querySelector("#exercise");
+const main = document.querySelector("main");
+const exerciseSection = document.querySelector(".exercise-section");
+let infoEl = document.querySelector("#info");
